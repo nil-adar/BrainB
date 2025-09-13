70e51dcff01fab17eda05cce2e963891c65e6276
nil-adar <niladar@gamil.com>
2025-09-13 22:18:42 +0300

    Fix

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 
 const isProduction = import.meta.env.PROD;
-
+// update pro
 // אם זה פרודקשן  משתמשים בשרת בויריילו
 // אם זה פיתוח → נשארים עם localhost כדי שיהיה נוח לבדוק
 export const API_BASE_URL = isProduction
