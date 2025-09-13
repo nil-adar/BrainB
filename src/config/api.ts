16db7108480ef14b79c2420b0bc1260c3bf2870a
nil-adar <niladar@gamil.com>
2025-09-13 21:46:28 +0300

    Fix ERROR

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,8 @@
-export const API_BASE_URL = '/api';
+
+const isProduction = import.meta.env.PROD;
+
+// אם זה פרודקשן  משתמשים בשרת בויריילו
+// אם זה פיתוח → נשארים עם localhost כדי שיהיה נוח לבדוק
+export const API_BASE_URL = isProduction
+  ? "https://brainb-production.up.railway.app/api"
+  : "http://localhost:5000/api";
